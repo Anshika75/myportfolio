ea9b70ff8238e0514261ba0c200bc13af88d7395
Anshika <anshikathakur596@gmail.com>
2023-11-27 23:35:51 +0530

    themeselector page done

---
+++ src/app/Terminal/page.tsx
@@ -0,0 +1,18 @@
+// pages/terminal.tsx (similar pages for other themes)
+import React from 'react';
+import Head from 'next/head';
+
+const Terminal: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Terminal Mode</title>
+      </Head>
+      <h1>Terminal Mode</h1>
+      {/* Your terminal-specific content goes here */}
+      {/* <ThemeSelector /> */}
+    </div>
+  );
+};
+
+export default Terminal;
---
+++ src/app/ThemeSelector/page.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '../../styles/ThemeSelector/styles.module.css';
+import themes from '../../helpers/theme'
+
+interface themes {
+    name: string;
+    imgSrc: string;
+    description: string;
+    id: number;
+    url: string;
+}
+
+const ThemeSelector: React.FC = () => {
+
+
+    return (
+        <main className={`p-[20px] lg:p-[50px] ${styles.bg}`}>
+            <div className={styles.overlay}></div>
+            <div className="flex flex-col items-center z-20">
+                <div className={styles.head}><p className='text-[25px] lg:text-[90px] opacity-75'>Enter the Enclave</p></div>
+                <h2 className={styles.sub}><p className='text-[15px] lg:text-[35px] lg:-mt-[20px] opacity-75'>Plunge into Creative Realms</p></h2>
+                <div className='grid grid-cols-1 lg:grid-cols-4 gap-12 place-items-center mt-12 px-4'>
+                    {themes.map((theme) => (
+                        <Link href={theme.url} className='text-center'>
+                            <div className="box transition-all hover:scale-110 flex flex-col items-center pb-5 px-5 bg-[#3335] shadow-[0_0px_60px_-10px_rgba(51,51,51,1)] border-[2px] border-[#3335] rounded-xl">
+                                <img src={theme.imgSrc} className='h-36 w-36' alt="" />
+                                <p key={theme.id} className={`flex flex-col opacity-75 items-center ${styles.sub} text-[20px]`}>
+                                    {theme.name}
+                                </p>
+
+                                <p className={`text-[14px] mt-4 opacity-75 text-justify ${styles.text}`}>{theme.description}</p>
+                            </div>
+                        </Link>
+                    ))}
+
+                </div>
+            </div>
+        </main >
+    );
+};
+
+export default ThemeSelector;
+
