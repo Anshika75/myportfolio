57fbc565e54b39ec6a4e3797288d239248dbfa16
Anshika <anshikathakur596@gmail.com>
2023-11-29 12:47:57 +0530

    typing effect added

--- src/app/Terminal/page.tsx
+++ src/app/Terminal/page.tsx
@@ -31,19 +31,92 @@ const themes: { [key: string]: Theme } = {
     name: 'Dark',
     backgroundColor: '#000000',
     textColor: '#ffffff',
-    visitorTextColor: '#8aff80',
+    visitorTextColor: '##60fdff',
     authorTextColor: '#00ff9c',
     commandTextColor: '#ffffff',
   },
   light: {
     name: 'Light',
-    backgroundColor: '#ffffff',
+    backgroundColor: '#D3D7CF',
     textColor: '#000000',
-    visitorTextColor: '#008000',
-    authorTextColor: '#00ff9c',
+    visitorTextColor: '#3465A4',
+    authorTextColor: '#4E9A06',
     commandTextColor: '#000000',
   },
-  // Add more themes as needed
+  coffee: {
+    name: 'Coffee',
+    backgroundColor: '#3E2723',
+    textColor: '#D7CCC8',
+    visitorTextColor: '#FF8A80',
+    authorTextColor: '#D32F2F',
+    commandTextColor: '#D7CCC8',
+  },
+  mint: {
+    name: 'Mint',
+    backgroundColor: '#263238',
+    textColor: '#B0BEC5',
+    visitorTextColor: '#66BB6A',
+    authorTextColor: '#26A69A',
+    commandTextColor: '#B0BEC5',
+  },
+  forest: {
+    name: 'Forest',
+    backgroundColor: '#1B3C2D',
+    textColor: '#D9E5D6',
+    visitorTextColor: '#9BC53D',
+    authorTextColor: '#57A773',
+    commandTextColor: '#D9E5D6',
+  },
+  rainbow: {
+    name: 'Rainbow',
+    backgroundColor: '#1A1B34',
+    textColor: '#ADD8E6',
+    visitorTextColor: '#FF00FF',
+    authorTextColor: '#800080',
+    commandTextColor: '#008000',
+  },
+  ocean: {
+    name: 'Ocean',
+    backgroundColor: '#002A32',
+    textColor: '#B0C4DE',
+    visitorTextColor: '#00BFFF',
+    authorTextColor: '#20B2AA',
+    commandTextColor: '#87CEEB',
+
+  },
+  sunset: {
+    name: 'Sunset',
+    backgroundColor: '#1A1C1E',
+    textColor: '#faf0b9',
+    visitorTextColor: '#FF0000',
+    authorTextColor: '#FFA500',
+    commandTextColor: '#FFFF00',
+  },
+  charcoal: {
+    name: 'Charcoal',
+    backgroundColor: '#2F2F2F',
+    textColor: '#D3D3D3',
+    visitorTextColor: '#6495ED',
+    authorTextColor: '#F08080',
+    commandTextColor: '#98FB98',
+  },
+  nebula: {
+    name: 'Nebula',
+    backgroundColor: '#0B0C10',
+    textColor: '#C5C6C7',
+    visitorTextColor: '#00BFFF',
+    authorTextColor: '#FFD700',
+    commandTextColor: '#FF6347',
+  },
+  moonlit: {
+    name: 'Moonlit',
+    backgroundColor: '#0C0E16', 
+    textColor: '#A5B1C2',
+    visitorTextColor: '#678FE1', 
+    authorTextColor: '#F9D71C', 
+    commandTextColor: '#F76D57',
+  }
+
 };
 const asciiArt = `
     _           _    _ _        
@@ -76,17 +149,26 @@ const Terminal: React.FC = () => {
       if (enteredCommand.startsWith('theme')) {
         handleThemeCommand(enteredCommand);
       } else {
-        // Find the entered command in the commands data
         const matchedCommand = commands.find((cmd) => cmd.command === enteredCommand);
-
-        // Add the entered command and its description to the history
-        setCommandHistory((prevHistory) => [
-          ...prevHistory,
-          {
-            command: enteredCommand,
-            description: matchedCommand ? matchedCommand.description : 'Command not found',
-          },
-        ]);
+        if (matchedCommand) {
+          typeDescription(matchedCommand.description).then((typedDescription) => {
+            setCommandHistory((prevHistory) => [
+              ...prevHistory,
+              {
+                command: enteredCommand,
+                description: typedDescription,
+              },
+            ]);
+          });
+        } else {
+          setCommandHistory((prevHistory) => [
+            ...prevHistory,
+            {
+              command: enteredCommand,
+              description: 'Command not found',
+            },
+          ]);
+        }
 
         // Process the entered command (you can add more logic here)
         console.log(`Command entered: ${enteredCommand}`);
@@ -96,23 +178,52 @@ const Terminal: React.FC = () => {
       setInputValue('');
     }
   };
-
+  const typeDescription = async (description: string): Promise<string> => {
+    const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+  
+    let typedDescription = '';
+  
+    for (let i = 0; i < description.length; i++) {
+      typedDescription += description[i   ];
+      setCommandHistory((prevHistory) => [
+        ...prevHistory.slice(0, 1),
+        {
+          command: prevHistory[prevHistory.length - 1]?.command || '',
+          description: typedDescription,
+        },
+      ]);
+  
+      await delay(5); // Adjust the delay as needed
+    }
+  
+    return typedDescription;
+  };
+  
   const handleThemeCommand = (command: string) => {
     const themeName = command.split(' ')[1];
     const selectedTheme = themes[themeName];
-
+  
     if (selectedTheme) {
       setCurrentTheme(selectedTheme);
+      setCommandHistory((prevHistory) => [
+        ...prevHistory,
+        {
+          command,
+          description: `Switched to theme "${themeName}"\n`,
+        },
+      ]);
     } else {
       setCommandHistory((prevHistory) => [
         ...prevHistory,
         {
           command,
-          description: `Theme "${themeName}" not found`,
+          description: `Theme "${themeName}" not found\n`,
         },
       ]);
     }
   };
+ 
+  
 
   useEffect(() => {
     // Focus on the input when the component mounts
@@ -128,13 +239,13 @@ const Terminal: React.FC = () => {
         <div className="h-full">
 
           <div>
-            <span className="font-medium" style={{ color: currentTheme.visitorTextColor }}>
+            <span className="font-semibold" style={{ color: currentTheme.visitorTextColor }}>
               visitor
             </span>
-            <span className="font-medium" style={{ color: currentTheme.authorTextColor }}>
+            <span className="font-semibold" style={{ color: currentTheme.authorTextColor }}>
               @terminalanshika
             </span>
-            <span className="font-medium" style={{ color: currentTheme.commandTextColor }}>
+            <span className="font-semibold" style={{ color: currentTheme.commandTextColor }}>
               :~$ welcome <br />
             </span>
             <pre>
@@ -165,13 +276,13 @@ const Terminal: React.FC = () => {
           {commandHistory.map((command, index) => (
             <div key={index}>
               <span>
-                <span className="font-medium" style={{ color: currentTheme.visitorTextColor }}>
+                <span className="font-semibold" style={{ color: currentTheme.visitorTextColor }}>
                   visitor
                 </span>
-                <span className="font-medium" style={{ color: currentTheme.authorTextColor }}>
+                <span className="font-semibold" style={{ color: currentTheme.authorTextColor }}>
                   @terminalanshika
                 </span>
-                <span className="font-medium" style={{ color: currentTheme.commandTextColor }}>
+                <span className="font-semibold" style={{ color: currentTheme.commandTextColor }}>
                   :~$&nbsp;
                 </span>
                 {`${command.command}`}
@@ -185,13 +296,13 @@ const Terminal: React.FC = () => {
         </div>
         <div className="mt-4">
           <span>
-            <span className="font-medium" style={{ color: currentTheme.visitorTextColor }}>
+            <span className="font-semibold" style={{ color: currentTheme.visitorTextColor }}>
               visitor
             </span>
-            <span className="font-medium" style={{ color: currentTheme.authorTextColor }}>
+            <span className="font-semibold" style={{ color: currentTheme.authorTextColor }}>
               @terminalanshika
             </span>
-            <span className="font-medium" style={{ color: currentTheme.commandTextColor }}>
+            <span className="font-semibold" style={{ color: currentTheme.commandTextColor }}>
               :~$&nbsp;
             </span>
           </span>
@@ -202,6 +313,7 @@ const Terminal: React.FC = () => {
             onChange={handleInputChange}
             onKeyPress={handleKeyPress}
             className={styles.inputField}
+            style={{ color: currentTheme.commandTextColor }}
           />
         </div>
       </div>
