a123c1041eab8c74af87578cab7d04a0c4ad5a57
Anshika <anshikathakur596@gmail.com>
2023-12-25 19:56:35 +0530

    loader added

--- src/app/DoodlePage/page.tsx
+++ src/app/DoodlePage/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Landing from '@/components/DoodlePage/Home/landing';
 import About from '@/components/DoodlePage/Home/aboutme';
 import Skills from '@/components/DoodlePage/Home/skills';
@@ -9,30 +9,45 @@ import Volunteer from '@/components/DoodlePage/Home/volunteer';
 import Testimonial from '@/components/DoodlePage/Home/testimonial';
 import Stats from '@/components/DoodlePage/Home/stats';
 import Hobbies from '@/components/DoodlePage/Home/hobbies';
-import Achievments from '@/components/DoodlePage/Home/achievments';
+import Achievements from '@/components/DoodlePage/Home/achievments'; // Corrected spelling
 import Languages from '@/components/DoodlePage/Home/languages';
 import Contact from '@/components/DoodlePage/Home/contact';
-
+import Loader from '@/components/DoodlePage/Home/loader';
 
 const DoodlePage: React.FC = () => {
-        return (
-            <>
-            <div className="main w-full min-h-screen bg-white">
-                <Landing/>
-                <About/>
-                <Skills/>
-                <Experience/>
-                <Projects/>
-                <Volunteer/>
-                <Stats/>
-                <Testimonial/> 
-                <Hobbies/>
-                <Achievments/>
-                <Languages/>
-                <Contact/>
-            </div>
-            </>
-    );
+  const [contentLoaded, setContentLoaded] = useState(false);
+
+  useEffect(() => {
+    // Simulate loading time (you can replace this with actual data fetching logic)
+    const delay = setTimeout(() => {
+      setContentLoaded(true);
+    }, 2000);
+
+    return () => clearTimeout(delay); // Cleanup timeout on component unmount
+  }, []);
+
+  return (
+    <>
+      {!contentLoaded && <Loader />}
+
+      {contentLoaded && (
+        <div className="main w-full flex flex-col min-h-screen bg-white">
+          <Landing />
+          <About />
+          <Skills />
+          <Experience />
+          <Projects />
+          <Volunteer />
+          <Stats />
+          <Testimonial />
+          <Hobbies />
+          <Achievements />
+          <Languages />
+          <Contact />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default DoodlePage;
