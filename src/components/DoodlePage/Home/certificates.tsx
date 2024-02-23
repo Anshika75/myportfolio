04cc0659ef3eae8ba30f2b3a28e8d8c5b768ca52
Anshika <anshikathakur596@gmail.com>
2024-02-23 16:09:08 +0530

    ui updates

--- src/components/DoodlePage/Home/certificates.tsx
+++ src/components/DoodlePage/Home/certificates.tsx
@@ -15,31 +15,31 @@ const Certificates: React.FC = () => {
                 <div
                     className={`grid w-full pb-4 lg:pt-8 mt-8 grid-cols-1 gap-4`}
                 >
-                    <a href='https://coursera.org/share/7831a6f569d246d15db51baefef79cbb' className="flex flex-row items-center justify-between relative transition-all hover:scale-[0.92] scale-[0.9] cursor-pointer py-4 px-4 w-full border-4 border-black rounded-[75px]">
+                    <Link href='https://www.linkedin.com/learning/certificates/e5c81cad326867fa2577fc12b7ce39a3517cd4df597c87302b8ca2a7491f2747' className="flex flex-row items-center justify-between relative transition-all hover:scale-[0.92] scale-[0.9] cursor-pointer py-4 px-4 w-full border-4 border-black rounded-[75px]">
                         <div className="rounded-full lg:h-5 h-4 w-4 lg:w-5 bg-black relative before:absolute before:top-1/2 before:left-1/2 before:h-32 before:w-1 lg:before:w-2 before:-translate-x-1/2 before:rounded-3xl before:-translate-y-1/2 before:bg-black"></div>
                         <p className={`text-sm lg:text-3xl text-black z-20 text-center ${styles.pang}`}>
-                        Getting Started with Front-End and Web Development
+                            Introduction to Graphic Design: Concepts
                         </p>
                         <div className="rounded-full lg:h-5 h-4 w-4 lg:w-5 bg-black relative before:absolute before:top-1/2 before:left-1/2 before:h-32 before:w-1 lg:before:w-2 before:-translate-x-1/2 before:rounded-3xl before:-translate-y-1/2 before:bg-black"></div>
-                    </a>
-                    <a href='https://coursera.org/share/7831a6f569d246d15db51baefef79cbb' className="flex flex-row items-center justify-between relative transition-all hover:scale-[0.92] scale-[0.9] cursor-pointer py-4 px-4 w-full border-4 border-black rounded-[75px]">
+                    </Link>
+                    <Link href='https://www.linkedin.com/learning/certificates/f14717b8c703499d3c88a7abcaa86a37f53fe6b96e62de1bafd5c3440dc7eb9c' className="flex flex-row items-center justify-between relative transition-all hover:scale-[0.92] scale-[0.9] cursor-pointer py-4 px-4 w-full border-4 border-black rounded-[75px]">
                         <div className="rounded-full lg:h-5 h-4 w-4 lg:w-5 bg-black relative before:absolute before:top-1/2 before:left-1/2 before:h-32 before:w-1 lg:before:w-2 before:-translate-x-1/2 before:rounded-3xl before:-translate-y-1/2 before:bg-black"></div>
                         <p className={`text-sm lg:text-3xl text-black z-20 text-center ${styles.pang}`}>
-                        Getting Started with Front-End and Web Development
+                            Graphic Design Foundations: Typography
                         </p>
                         <div className="rounded-full lg:h-5 h-4 w-4 lg:w-5 bg-black relative before:absolute before:top-1/2 before:left-1/2 before:h-32 before:w-1 lg:before:w-2 before:-translate-x-1/2 before:rounded-3xl before:-translate-y-1/2 before:bg-black"></div>
-                    </a>
-                    <a href='https://coursera.org/share/7831a6f569d246d15db51baefef79cbb' className="flex flex-row items-center justify-between relative transition-all hover:scale-[0.92] scale-[0.9] cursor-pointer py-4 px-4 w-full border-4 border-black rounded-[75px]">
+                    </Link>
+                    <Link href='https://www.linkedin.com/learning/certificates/54b0c826b5e0cbb93dc7922358cf1b96bc0410f91de9331967a9e07e40dfd24d' className="flex flex-row items-center justify-between relative transition-all hover:scale-[0.92] scale-[0.9] cursor-pointer py-4 px-4 w-full border-4 border-black rounded-[75px]">
                         <div className="rounded-full lg:h-5 h-4 w-4 lg:w-5 bg-black relative before:absolute before:top-1/2 before:left-1/2 before:h-32 before:w-1 lg:before:w-2 before:-translate-x-1/2 before:rounded-3xl before:-translate-y-1/2 before:bg-black"></div>
                         <p className={`text-sm lg:text-3xl text-black z-20 text-center ${styles.pang}`}>
-                        Getting Started with Front-End and Web Development
+                            Graphic Design Foundations: Layout and Composition
                         </p>
                         <div className="rounded-full lg:h-5 h-4 w-4 lg:w-5 bg-black relative before:absolute before:top-1/2 before:left-1/2 before:h-32 before:w-1 lg:before:w-2 before:-translate-x-1/2 before:rounded-3xl before:-translate-y-1/2 before:bg-black"></div>
-                    </a>
+                    </Link>
                 </div>
-                
+
                 <Link href={'/DoodlePageCertificates'} className={`flex flex-col relative w-full my-12 items-center transition-all opacity-80 hover:opacity-100 hover:scale-95`}>
-                <img src="https://www.dropbox.com/scl/fi/ezi8w9x1a9ro2drli15hf/Group-5.png?rlkey=j19qmjvz2gjd6d5b62ifh6phg&dl=1" className="absolute top-[50%] left-[50%] h-[4rem] w-[12rem] -translate-x-1/2 -translate-y-1/2" />
+                    <img src="https://www.dropbox.com/scl/fi/ezi8w9x1a9ro2drli15hf/Group-5.png?rlkey=j19qmjvz2gjd6d5b62ifh6phg&dl=1" className="absolute top-[50%] left-[50%] h-[4rem] w-[12rem] -translate-x-1/2 -translate-y-1/2" />
                     <p className={`text-white text-2xl z-20 ${styles.pang}`}>Dig More</p>
                 </Link>
             </div>
--- src/components/DoodlePage/Home/event.tsx
+++ src/components/DoodlePage/Home/event.tsx
@@ -8,37 +8,37 @@ const Events: React.FC = () => {
 
             <div className="flex flex-col items-center justify-start lg:mt-16 px-[20px] lg:px-[60px]">
                 <div className={`flex flex-col relative w-full items-center scale-50 lg:scale-100 ${styles.anim7}`}>
-                    <img src="https://www.dropbox.com/scl/fi/jtt76wbmdsdp5tekwuoep/Group.png?rlkey=bn0nvhih0n4gkugd9zfennda1&dl=1" className="absolute top-[70%] left-[50%] h-[7rem] w-[10rem] -translate-x-1/2 -translate-y-1/2" />
-                    <p className={`text-white text-5xl z-20 mt-12 ${styles.pang}`}>Event</p>
+                    <img src="https://www.dropbox.com/scl/fi/jtt76wbmdsdp5tekwuoep/Group.png?rlkey=bn0nvhih0n4gkugd9zfennda1&dl=1" className="absolute top-[70%] left-[50%] h-[5rem] w-[7rem] lg:h-[7rem] lg:w-[10rem] -translate-x-1/2 -translate-y-1/2" />
+                    <p className={`text-white text-xl lg:text-5xl z-20 mt-6 lg:mt-12 ${styles.pang}`}>Event</p>
                 </div>
                 <h1 className={`text-5xl lg:text-[120px] lg:-mt-4 text-black ${styles.lond} ${styles.anim2} font-black`}>Roster</h1>
                 <div
-                    className={`grid w-full pt-16 lg:pt-10 lg:grid-cols-3 grid-cols-1 gap-4`}
+                    className={`grid w-full pt-8 pb-20 lg:pb-0 lg:pt-10 lg:grid-cols-3 grid-cols-1 gap-4`}
                 >
                     <div className="flex flex-col items-center justify-center relative py-10 w-full">
                         <img src="https://www.dropbox.com/scl/fi/ph904qlgj6ms9ijx4al6j/Group-9.png?rlkey=4kbra8h0cvq39ik9cs7fovvz3&dl=1" className='absolute top-0 left-0 w-full h-full' alt="" />
-                        <p className={`text-xl lg:text-3xl text-white z-20 text-center ${styles.perm}`}>
+                        <p className={`text-xl lg:text-3xl text-white z-20 text-center ${styles.pang}`}>
                             HashCode
                         </p>
-                        <p className={`text-xl lg:text-4xl lg:mt-3 text-white z-20 text-center ${styles.pang}`}>
+                        <p className={`text-xl lg:text-4xl lg:mt-3 text-white z-20 text-center ${styles.perm}`}>
                             2022
                         </p>
                     </div>
                     <div className="flex flex-col items-center justify-center relative py-10 w-full">
                         <img src="https://www.dropbox.com/scl/fi/ph904qlgj6ms9ijx4al6j/Group-9.png?rlkey=4kbra8h0cvq39ik9cs7fovvz3&dl=1" className='absolute top-0 left-0 w-full h-full' alt="" />
-                        <p className={`text-lg lg:text-2xl text-white z-20 text-center ${styles.perm}`}>
+                        <p className={`text-lg lg:text-2xl text-white z-20 text-center ${styles.pang}`}>
                             &nbsp;&nbsp;Hack-The-Fest
                         </p>
-                        <p className={`text-xl lg:text-4xl lg:mt-3 text-white z-20 text-center ${styles.pang}`}>
+                        <p className={`text-xl lg:text-4xl lg:mt-3 text-white z-20 text-center ${styles.perm}`}>
                             2022
                         </p>
                     </div>
                     <div className="flex flex-col items-center justify-center relative py-10 w-full">
                         <img src="https://www.dropbox.com/scl/fi/ph904qlgj6ms9ijx4al6j/Group-9.png?rlkey=4kbra8h0cvq39ik9cs7fovvz3&dl=1" className='absolute top-0 left-0 w-full h-full' alt="" />
-                        <p className={`text-xl lg:text-3xl text-white z-20 text-center ${styles.perm}`}>
+                        <p className={`text-xl lg:text-3xl text-white z-20 text-center ${styles.pang}`}>
                             HS Day
                         </p>
-                        <p className={`text-xl lg:text-4xl lg:mt-3 text-white z-20 text-center ${styles.pang}`}>
+                        <p className={`text-xl lg:text-4xl lg:mt-3 text-white z-20 text-center ${styles.perm}`}>
                             2023
                         </p>
                     </div>
