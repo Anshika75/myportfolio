5773c8f3073932e4f738ddded707f6858f116953
Anshika <anshikathakur596@gmail.com>
2024-02-27 11:34:29 +0530

    Update page.tsx

--- src/app/ThemeSelector/page.tsx
+++ src/app/ThemeSelector/page.tsx
@@ -18,8 +18,7 @@ const ThemeSelector: React.FC = () => {
   const closeAlert = () => {
     setShowAlert(false);
   };
-
-  const isTerminalModeAvailable = false;
+x
 
   return (
     <main className={`p-[20px] lg:p-[50px] ${styles.bg}`}>
