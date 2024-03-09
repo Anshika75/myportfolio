1a2d84a17979d8ae30b1f723ac308b87f5b4c393
Anshika <anshikathakur596@gmail.com>
2024-03-09 10:37:49 +0530

    Create loader.tsx

---
+++ src/components/DoodlePageDark/loader.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styles from '../../styles/DoodlePage/style.module.css';
+
+export default function Loader() {
+    
+    return (
+        <>
+            <div className={`${styles.main}`}>
+                
+            <div className={`${styles.hexagon}`}>
+                <div className={`${styles.hexagongroup}`}>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                </div>
+                <div className={`${styles.hexagongroup}`}>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                </div>
+                <div className={`${styles.hexagongroup}`}>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                </div>
+                <div className={`${styles.hexagongroup}`}>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                </div>
+                <div className={`${styles.hexagongroup}`}>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                </div>
+                <div className={`${styles.hexagongroup}`}>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                    <div className={`${styles.hexagonsector}`}></div>
+                </div>
+            </div>
+            <p aria-label={`${styles.Loading}`}>Loading</p>
+            </div>
+        </>
+    )
+}
