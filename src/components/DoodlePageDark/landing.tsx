df705c662675a775a4430eda5272c3b1d2bf3b3d
Anshika <anshikathakur596@gmail.com>
2024-03-09 15:07:06 +0530

    Update landing.tsx

--- src/components/DoodlePageDark/landing.tsx
+++ src/components/DoodlePageDark/landing.tsx
@@ -12,7 +12,17 @@ const Landing: React.FC = () => {
             justifyContent: 'center',
             alignItems: 'center',
             width: '100%',
+            overflow: 'hidden',
+        },
+        bg: {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: "url('https://i.gifer.com/QON.gif')",
             backgroundSize: 'cover',
+            filter: 'blur(5px)',
             overflow: 'hidden',
         },
     };
@@ -20,7 +30,8 @@ const Landing: React.FC = () => {
     return (
         <>
             <div style={style.container} className='h-[70vh] lg:min-h-[100vh] bg-black'>
-                <div className="flex flex-col justify-center items-center">
+                <div style={style.bg} className="absolute top-0 left-0 w-full h-full bg-black opacity-80"></div>
+                <div className="z-20 flex flex-col justify-center items-center opacity-90">
                     <div className={`rounded-full mb-6 w-36 h-36 lg:w-60 lg:h-60 relative grid place-items-center bg-red-100`}>
                         <img className={`absolute animate-pulse w-[75%] h-[75%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full z-30`} src="https://www.dropbox.com/scl/fi/12q4wpy092tc6mv9urfmh/Abstract_Pink_Gamer_Girl_Gaming_Logo-removebg-preview-1.png?rlkey=tvjpg7ol90tl8jghvwniaughr&dl=1" alt="" />
                         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full h-[95%] w-[95%] bg-black z-10"></div>
