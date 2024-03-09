3cc66af7f43bfb9d93bc046448093f371d78a942
Anshika <anshikathakur596@gmail.com>
2024-03-09 10:39:27 +0530

    Update page.tsx

--- src/app/DoodlePage/page.tsx
+++ src/app/DoodlePage/page.tsx
@@ -1,21 +1,7 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import Landing from '@/components/DoodlePage/landing';
-import About from '@/components/DoodlePage/aboutme';
-import Skills from '@/components/DoodlePage/skills';
-import Experience from '@/components/DoodlePage/experience';
-import Projects from '@/components/DoodlePage/projects';
-import Volunteer from '@/components/DoodlePage/volunteer';
-import Testimonial from '@/components/DoodlePage/testimonial';
-import Stats from '@/components/DoodlePage/stats';
-import Hobbies from '@/components/DoodlePage/hobbies';
-import Achievements from '@/components/DoodlePage/achievments';
-import Languages from '@/components/DoodlePage/languages';
-import Contact from '@/components/DoodlePage/contact';
-import Loader from '@/components/DoodlePage/loader';
-import Badges from '@/components/DoodlePage/badges';
-import Events from '@/components/DoodlePage/event';
-import Certificates from '@/components/DoodlePage/certificates';
+import Landing from '@/components/DoodlePageDark/landing';
+import Loader from '@/components/DoodlePageDark/loader';
 
 const DoodlePageDark: React.FC = () => {
   const [contentLoaded, setContentLoaded] = useState(false);
@@ -41,20 +27,6 @@ const DoodlePageDark: React.FC = () => {
       {contentLoaded && (
         <div className="main w-full flex flex-col min-h-screen bg-white">
           <Landing/>
-          <About/>
-          <Skills />
-          <Experience />
-          <Projects />
-          <Certificates/>
-          <Badges />
-          <Volunteer />
-          <Events/>
-          <Stats />
-          <Testimonial />
-          <Hobbies />
-          <Achievements />
-          <Languages />
-          <Contact />
         </div>
       )}
     </>
