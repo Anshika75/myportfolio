df33d8e08a61d73f5723e7156c3649ecf48799db
anshika75 <anshikathakur596@gmail.com>
2024-11-02 02:06:23 +0530

    Update experience.js

--- src/helpers/experience.js
+++ src/helpers/experience.js
@@ -1,77 +1,90 @@
 // experience.js
 const experiences = [
-    {
-      "company": "Cu Academic Competitions",
-      "role": "Development Executive",
-      "timePeriod": "Dec 2023 - Present",
-      "responsibilities": "Designed the app on Figma and developed website. Along with that I've contributed to event and team management",
-      "url": "https://www.dropbox.com/scl/fi/gpxdga978r7h7imklxz2p/logonav-oMzIXFFe.png?rlkey=7lwxpm1zqfi5s2jxe1pzfo3kk&dl=1"
-    },
-    {
-      "company": "Medantrik Medtech Pvt Ltd",
-      "role": "Frontend Developer",
-      "timePeriod": "Oct 2023 - Present",
-      "responsibilities": "Designed the app on Figma and developed its working prototype.",
-      "url": "https://www.dropbox.com/scl/fi/a3y6grw2m3yw1p2xot34o/medantrik_logo.jpeg?rlkey=uls9r4v1fw0wltreuay5i02nl&dl=1"
-    },
-    {
-      "company": "Report Prime",
-      "role": "Web Development Intern",
-      "timePeriod": "Jun 2023 - Oct 2023",
-      "responsibilities": "Developed the site using Bootstrap and Bootstrap Sass.",
-      "url": "https://www.dropbox.com/scl/fi/tvwvajw8w4mksvmkc3n9y/reportmines_com_logo-removebg-preview.png?rlkey=8iz60k117fffu1q594dhydd4y&dl=1"
-    },
-    {
-      "company": "Hackoverflow Society",
-      "role": "Technical Lead",
-      "timePeriod": "Jan 2022 - Aug 2023",
-      "responsibilities": "Led the tech team, developed the team website, and actively contributed to event organization. Organized HS Day, Hack The Fest, and judged the CSS Quest competition.",
-      "url": "https://www.dropbox.com/scl/fi/8gkb7gvqroajvr6ms05yj/images__1_-removebg-preview.png?rlkey=k2w58h6bb1bqk3k55u8dkxip3&dl=1"
-    },
-    {
-      "company": "House of Couton Private Limited",
-      "role": "Subject Matter Expert",
-      "timePeriod": "Jun 2023 - Jul 2023",
-      "responsibilities": "Completed content writing tasks, worked with UML software, and did wireframe designs.",
-      "url": "https://www.dropbox.com/scl/fi/r89a4yh2zsqxmlpfhddif/1630604650807-removebg-preview.png?rlkey=hfjf3wkf8xg10t2bjjb29ld0d&dl=1"
-    },
-    {
-      "company": "INFINITY SMART INDIA",
-      "role": "Web Developer",
-      "timePeriod": "Jun 2022 - Dec 2022",
-      "responsibilities": "Developed a school management system on React and Tailwind from Figma UI.",
-      "url": "https://www.dropbox.com/scl/fi/ez91wyf6akk1h9c0gwgim/1649053371963-removebg-preview.png?rlkey=o6wphu6kupdw6ubewy0gy2w5o&dl=1"
-    },
-    {
-      "company": "Repaybly",
-      "role": "Senior Frontend Developer",
-      "timePeriod": "Aug 2022 - Nov 2022",
-      "responsibilities": "Developed the website using React and Tailwind; managed the team.",
-      "url": "https://www.dropbox.com/scl/fi/pkla3l9pgyq1mg06ostu0/repaybly_gmz_logo-removebg-preview.png?rlkey=vahi3jxc8lec488rqsw6b6w6x&dl=1"
-    },
-    {
-      "company": "TreeAndHumanKnot",
-      "role": "Web Development Intern",
-      "timePeriod": "Jun 2022 - Sep 2022",
-      "responsibilities": "Designed and developed the website using child and parent themes.",
-      "url": "https://www.dropbox.com/scl/fi/y2qpy5fhqe38fwpy04nz1/images-2.jpeg?rlkey=9s7bto0cygbryr2zj7yvn4ylv&dl=1"
-    },
-    {
-      "company": "The LaDebug Club",
-      "role": "Technical Team Co-ordinator",
-      "timePeriod": "Jan 2022 - Aug 2022",
-      "responsibilities": "Developed the team website and handled mailer management.",
-      "url": "https://www.dropbox.com/scl/fi/08uksa8dcopwimxu0bo1u/society_club_official_logo-removebg-preview.png?rlkey=4829g3fhqeroode68ht7jy1mo&dl=1"
-    },
-    {
-      "company": "BasketHunt Pvt Ltd",
-      "role": "Internet Technology Development Intern",
-      "timePeriod": "Feb 2022 - Apr 2022",
-      "responsibilities": "Managed VPNs and user profiles on the Moodle platform.",
-      "url": "https://www.dropbox.com/scl/fi/789q3c0lzplgo0uu0uy7v/1630649608507-removebg-preview.png?rlkey=vwjihkvnpu6re98h03vas0dkt&dl=1"
-    }
-  ];
-  
-  
-  export default experiences;
-  
+  {
+    "company": "FICO",
+    "role": "Software Developer Intern",
+    "timePeriod": "Sep 2024 - Present",
+    "responsibilities": "Working on angular projects.",
+    "url": "https://www.dropbox.com/scl/fi/h1phts4rpcd3pv2ftqe0n/fico_logo-1.jpeg?rlkey=9jn0931o0xd4mvzjo0d6pca58&st=85a5pr0f&dl=1"
+  },
+  {
+    "company": "Embsoft Products Research & Development LLP",
+    "role": "Web Developer",
+    "timePeriod": "May 2023 - Aug 2024",
+    "responsibilities": "Designed projects from scratch, developed websites and managed MVP projects.",
+    "url": "https://www.dropbox.com/scl/fi/gn7ondw94b5fccwr0ifhf/1630616622252-1.jpeg?rlkey=t83picuzjkh6slxlykytvbm29&st=1sj8wdy7&dl=1"
+  },
+  {
+    "company": "Cu Academic Competitions",
+    "role": "Development Executive",
+    "timePeriod": "Dec 2023 - May 2024",
+    "responsibilities": "Designed the app on Figma and developed website. Along with that I've contributed to event and team management",
+    "url": "https://www.dropbox.com/scl/fi/gpxdga978r7h7imklxz2p/logonav-oMzIXFFe.png?rlkey=7lwxpm1zqfi5s2jxe1pzfo3kk&dl=1"
+  },
+  {
+    "company": "Medantrik Medtech Pvt Ltd",
+    "role": "Frontend Developer",
+    "timePeriod": "Oct 2023 - Present",
+    "responsibilities": "Designed the app on Figma and developed its working prototype.",
+    "url": "https://www.dropbox.com/scl/fi/a3y6grw2m3yw1p2xot34o/medantrik_logo.jpeg?rlkey=uls9r4v1fw0wltreuay5i02nl&dl=1"
+  },
+  {
+    "company": "Report Prime",
+    "role": "Web Development Intern",
+    "timePeriod": "Jun 2023 - Oct 2023",
+    "responsibilities": "Developed the site using Bootstrap and Bootstrap Sass.",
+    "url": "https://www.dropbox.com/scl/fi/tvwvajw8w4mksvmkc3n9y/reportmines_com_logo-removebg-preview.png?rlkey=8iz60k117fffu1q594dhydd4y&dl=1"
+  },
+  {
+    "company": "Hackoverflow Society",
+    "role": "Technical Lead",
+    "timePeriod": "Jan 2022 - Aug 2023",
+    "responsibilities": "Led the tech team, developed the team website, and actively contributed to event organization. Organized HS Day, Hack The Fest, and judged the CSS Quest competition.",
+    "url": "https://www.dropbox.com/scl/fi/8gkb7gvqroajvr6ms05yj/images__1_-removebg-preview.png?rlkey=k2w58h6bb1bqk3k55u8dkxip3&dl=1"
+  },
+  {
+    "company": "House of Couton Private Limited",
+    "role": "Subject Matter Expert",
+    "timePeriod": "Jun 2023 - Jul 2023",
+    "responsibilities": "Completed content writing tasks, worked with UML software, and did wireframe designs.",
+    "url": "https://www.dropbox.com/scl/fi/r89a4yh2zsqxmlpfhddif/1630604650807-removebg-preview.png?rlkey=hfjf3wkf8xg10t2bjjb29ld0d&dl=1"
+  },
+  {
+    "company": "INFINITY SMART INDIA",
+    "role": "Web Developer",
+    "timePeriod": "Jun 2022 - Dec 2022",
+    "responsibilities": "Developed a school management system on React and Tailwind from Figma UI.",
+    "url": "https://www.dropbox.com/scl/fi/ez91wyf6akk1h9c0gwgim/1649053371963-removebg-preview.png?rlkey=o6wphu6kupdw6ubewy0gy2w5o&dl=1"
+  },
+  {
+    "company": "Repaybly",
+    "role": "Senior Frontend Developer",
+    "timePeriod": "Aug 2022 - Nov 2022",
+    "responsibilities": "Developed the website using React and Tailwind; managed the team.",
+    "url": "https://www.dropbox.com/scl/fi/pkla3l9pgyq1mg06ostu0/repaybly_gmz_logo-removebg-preview.png?rlkey=vahi3jxc8lec488rqsw6b6w6x&dl=1"
+  },
+  {
+    "company": "TreeAndHumanKnot",
+    "role": "Web Development Intern",
+    "timePeriod": "Jun 2022 - Sep 2022",
+    "responsibilities": "Designed and developed the website using child and parent themes.",
+    "url": "https://www.dropbox.com/scl/fi/y2qpy5fhqe38fwpy04nz1/images-2.jpeg?rlkey=9s7bto0cygbryr2zj7yvn4ylv&dl=1"
+  },
+  {
+    "company": "The LaDebug Club",
+    "role": "Technical Team Co-ordinator",
+    "timePeriod": "Jan 2022 - Aug 2022",
+    "responsibilities": "Developed the team website and handled mailer management.",
+    "url": "https://www.dropbox.com/scl/fi/08uksa8dcopwimxu0bo1u/society_club_official_logo-removebg-preview.png?rlkey=4829g3fhqeroode68ht7jy1mo&dl=1"
+  },
+  {
+    "company": "BasketHunt Pvt Ltd",
+    "role": "Internet Technology Development Intern",
+    "timePeriod": "Feb 2022 - Apr 2022",
+    "responsibilities": "Managed VPNs and user profiles on the Moodle platform.",
+    "url": "https://www.dropbox.com/scl/fi/789q3c0lzplgo0uu0uy7v/1630649608507-removebg-preview.png?rlkey=vwjihkvnpu6re98h03vas0dkt&dl=1"
+  }
+];
+
+
+export default experiences;
